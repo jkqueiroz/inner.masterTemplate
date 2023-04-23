1cb88f57f7c7faeca0d859c50c2a381b78ba0fb8
k2.joao <joao.queiroz.external@k2partnering.com>
2023-04-22 23:15:06 -0300

    First Commit Front

---
+++ reactjs/src/routes.js
@@ -0,0 +1,78 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.3.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+import Notifications from "views/Notifications.js";
+import Icons from "views/Icons.js";
+import Typography from "views/Typography.js";
+import TableList from "views/Tables.js";
+import UserPage from "views/User.js";
+import Cadastro from "views/Cadastro.js";
+
+var routes = [
+  {
+    path: "/tables",
+    name: "Clientes",
+    icon: "nc-icon nc-tile-56",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/Cadastro",
+    name: "Devedores",
+    icon: "nc-icon nc-laptop",
+    component: Cadastro,
+    layout: "/admin"
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "nc-icon nc-bank",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "nc-icon nc-diamond",
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "nc-icon nc-bell-55",
+    component: Notifications,
+    layout: "/admin"
+  },
+  {
+    path: "/user-page",
+    name: "User Profile",
+    icon: "nc-icon nc-single-02",
+    component: UserPage,
+    layout: "/admin"
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    icon: "nc-icon nc-caps-small",
+    component: Typography,
+    layout: "/admin"
+  }
+];
+export default routes;
---
+++ reactjs/src/views/Cadastro.js
@@ -0,0 +1,176 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.3.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    CardTitle,
+    FormGroup,
+    Form,
+    Input,
+    Row,
+    Col
+} from "reactstrap";
+
+function Tables() {
+  return (
+    <>
+      <div className="content">
+        <Row>
+        <Col md="12">
+            <Card className="card-user">
+              <CardHeader>
+                <CardTitle tag="h5">Cadastro de Devedores</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                  <Row>
+                    <Col className="pr-1" md="4">
+                      <FormGroup>
+                        <label>Nome do Devedor</label>
+                        <Input
+                          placeholder="Nome"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pm-1" md="4">
+                      <FormGroup>
+                        <label htmlFor="exampleInputEmail1">
+                          Email
+                        </label>
+                        <Input placeholder="Email" type="email" />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-1" md="4">
+                      <FormGroup>
+                        <label htmlFor="exampleInputEmail1">
+                          Telefone
+                        </label>
+                        <Input placeholder="Telefone" type="Telefone" />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="pr-1" md="6">
+                      <FormGroup>
+                        <label>CPF</label>
+                        <Input
+                          defaultValue="CPF"
+                          placeholder="Company"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-1" md="6">
+                      <FormGroup>
+                        <label>RG</label>
+                        <Input
+                          defaultValue="RG"
+                          placeholder="Last Name"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md="12">
+                      <FormGroup>
+                        <label>Endereço</label>
+                        <Input
+                          placeholder="Endereço"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="pr-1" md="4">
+                      <FormGroup>
+                        <label>Cidade</label>
+                        <Input
+                          placeholder="Cidade"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="px-1" md="4">
+                      <FormGroup>   
+                        <label>Estado</label>
+                        <Input
+                        placeholder="Estado"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-1" md="4">
+                      <FormGroup>
+                        <label>CEP</label>
+                        <Input placeholder="CEP" type="number" />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md="12">
+                      <FormGroup>
+                        <label>Valor da Divida</label>
+                        <Input
+                            defaultValue="R$ "
+                            type="Text"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col md="12">
+                      <FormGroup>
+                        <label>Comentários</label>
+                        <Input
+                          type="textarea"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <div className="update ml-auto mr-auto">
+                      <Button
+                        className="btn-round"
+                        color="primary"
+                        type="submit"
+                      >
+                        Incluir
+                      </Button>
+                    </div>
+                  </Row>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Tables;
